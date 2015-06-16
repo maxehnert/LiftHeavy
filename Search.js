1a01744669816346da409da9cf7d1d1c1c25601b
Max Ehnert <max.ehnert@gmail.com>
2015-06-16 18:05:14 -0400

    init commit

---
+++ Search.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var React = require('react-native');
+var SearchBooks = require('./SearchBooks');
+
+var {
+    StyleSheet,
+    NavigatorIOS,
+    Component
+   } = React;
+
+var styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+});
+
+class Search extends Component {
+    render() {
+        return (
+            <NavigatorIOS
+                style={styles.container}
+                initialRoute={{
+            title: 'Search Weights',
+            component: SearchBooks
+        }}/>
+      );
+  }
+}
+
+module.exports = Search;
